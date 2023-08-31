6d94213090ce80ec83611f57ffdaec2a738c6d21
Jakub Jendryka <jjendryka@internships.antmicro.com>
2023-08-31 15:44:10 +0200

    Added install script

---
+++ script/install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+ci_flag=
+while getopts c name
+do
+    case $name in
+    c) ci_flag=1;;
+    ?) echo "Usage: $0: [-c]"
+        exit 2;;
+    esac
+done
+
+echo "This is ems-kicad installation script"
+
+# Install python
+if [[ -z "${ci_flag}" ]]; then
+    sudo apt-get -qq install python3 python3-pip python3-venv
+else
+    apt-get -qqy install python3 python3-pip python3-venv
+fi
+
+# Create ems-kicad.sh
+WD=$(pwd)
+echo '#!/usr/bin/env bash' > ems-kicad.sh
+echo "python3 $WD/main.py \"\$@\"">> ems-kicad.sh
+
+# Make kmake.sh executable
+chmod +x ems-kicad.sh
+
+# Create kmake command in "$HOME/.local/bin/"
+mkdir --parents "$HOME/.local/bin/"
+ln -sf "$(pwd)/ems-kicad.sh" "$HOME/.local/bin/ems-kicad"
+
+echo "Installed successfully. Type ems-kicad everywhere you want"
