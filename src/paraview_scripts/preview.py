7b37b227726360bacf262bb56e6af51722f23205
Pawel Koszalka <pkoszalka@internships.antmicro.com>
2025-01-22 10:49:13 +0100

    [#71941] Wrap preview into functions

--- src/paraview_scripts/preview.py
+++ src/paraview_scripts/preview.py
@@ -1,32 +1,41 @@
 import os
 from paraview.simple import *
 
-path = os.getcwd() + "/ems/simulation/0"
-files = os.listdir(path)
 
-files = [path + "/" + file for file in files if file[-4:] == ".vtr"]
+def get_sim_results(layer: str) -> list[str]:
+    path = os.getcwd() + "/ems/simulation/" + layer
+    files = os.listdir(path)
 
-paraview.simple._DisableFirstRenderCameraReset()
+    files = [path + "/" + file for file in files if file[-4:] == ".vtr"]
+    return files
 
-e_field = XMLRectilinearGridReader(registrationName="e_field", FileName=files)
-e_field.PointArrayStatus = ["E-Field"]
 
-animationScene = GetAnimationScene()
+def run_preview(files: list[str]) -> None:
+    paraview.simple._DisableFirstRenderCameraReset()
 
-animationScene.UpdateAnimationUsingDataTimeSteps()
+    e_field = XMLRectilinearGridReader(registrationName="e_field", FileName=files)
+    e_field.PointArrayStatus = ["E-Field"]
 
-renderView = GetActiveViewOrCreate("RenderView")
+    animationScene = GetAnimationScene()
 
-e_field_vtrDisplay = Show(e_field, renderView, "UniformGridRepresentation")
+    animationScene.UpdateAnimationUsingDataTimeSteps()
 
-ColorBy(e_field_vtrDisplay, ("POINTS", "E-Field", "Magnitude"))
+    renderView = GetActiveViewOrCreate("RenderView")
 
-e_field_vtrDisplay.RescaleTransferFunctionToDataRange(True, False)
+    e_field_vtrDisplay = Show(e_field, renderView, "UniformGridRepresentation")
 
-e_field_vtrDisplay.SetScalarBarVisibility(renderView, True)
+    ColorBy(e_field_vtrDisplay, ("POINTS", "E-Field", "Magnitude"))
 
-renderView.Update()
+    e_field_vtrDisplay.RescaleTransferFunctionToDataRange(True, False)
 
-renderView.ResetCamera(False)
+    e_field_vtrDisplay.SetScalarBarVisibility(renderView, True)
 
-animationScene.Play()
+    renderView.Update()
+
+    renderView.ResetCamera(False)
+
+    animationScene.Play()
+
+
+files = get_sim_results("0")
+run_preview(files)
