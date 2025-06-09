c1a7f754733395af3f499acedf34b3f458ead886
Dawid Lentka <dlentka@antmicro.com>
2025-06-09 08:56:29 +0200

    [#78639] ems2paraview: Import data as separate fields

--- src/ems2paraview/paraview_preview.py
+++ src/ems2paraview/paraview_preview.py
@@ -4,6 +4,7 @@
 import os
 from pathlib import Path
 from paraview.simple import *  # type: ignore #noqa: F403
+from typing import List, Dict
 
 
 def get_sim_results(port: str) -> list[str]:
@@ -16,26 +17,33 @@ def run_preview(files: list[str]) -> None:
     """Set up and run preview."""
     paraview.simple._DisableFirstRenderCameraReset()  # type: ignore #noqa: F405
 
-    e_field = XMLRectilinearGridReader(registrationName="e_field", FileName=files)  # type: ignore #noqa: F405
-    e_field.PointArrayStatus = ["E-Field"]
-
-    animation_scene = GetAnimationScene()  # type: ignore #noqa: F405
-
-    animation_scene.UpdateAnimationUsingDataTimeSteps()
-
     render_view = GetActiveViewOrCreate("RenderView")  # type: ignore #noqa: F405
+    grouped_files: Dict[str, List[str]] = {}
+    for file in files:
+        dump_name = Path(file).stem.rstrip("0123456789").rstrip("_")
+        gf = grouped_files.setdefault(dump_name, [])
+        gf.append(file)
+        grouped_files[dump_name] = gf
 
-    e_field_vtr_display = Show(e_field, render_view, "UniformGridRepresentation")  # type: ignore #noqa: F405
+    for dump_name, f in sorted(grouped_files.items()):
+        e_field = XMLRectilinearGridReader(registrationName=dump_name, FileName=sorted(f))  # type: ignore #noqa: F405
+        e_field.PointArrayStatus = ["E-Field"]
 
-    ColorBy(e_field_vtr_display, ("POINTS", "E-Field", "Magnitude"))  # type: ignore #noqa: F405
+        animation_scene = GetAnimationScene()  # type: ignore #noqa: F405
 
-    e_field_vtr_display.RescaleTransferFunctionToDataRange(True, False)
+        animation_scene.UpdateAnimationUsingDataTimeSteps()
 
-    e_field_vtr_display.SetScalarBarVisibility(render_view, True)
+        e_field_vtr_display = Show(e_field, render_view, "UniformGridRepresentation")  # type: ignore #noqa: F405
 
-    render_view.Update()
+        ColorBy(e_field_vtr_display, ("POINTS", "E-Field", "Magnitude"))  # type: ignore #noqa: F405
 
-    render_view.ResetCamera(False)
+        e_field_vtr_display.RescaleTransferFunctionToDataRange(True, False)
+
+        e_field_vtr_display.SetScalarBarVisibility(render_view, True)
+
+        render_view.Update()
+
+        render_view.ResetCamera(False)
 
     animation_scene.Play()
 
